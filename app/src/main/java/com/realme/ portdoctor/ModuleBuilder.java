004172c48dac382f6ac37dc6b24add73a1169242
Binit <jaiswalbinit605@gmail.com>
2026-06-14 21:37:10 +0530

    Update ModuleBuilder.java

--- app/src/main/java/com/realme/ portdoctor/ModuleBuilder.java
+++ app/src/main/java/com/realme/ portdoctor/ModuleBuilder.java
@@ -6,13 +6,21 @@ import java.util.zip.*;
 
 public class ModuleBuilder {
 
-    public static File buildModule(List<FixEngine.Fix> fixes, String outputPath) throws Exception {
-        File moduleDir = new File("/sdcard/PortDoctor_Module");
+    /**
+     * Build a Magisk module zip in the given writable directory.
+     * @param fixes     List of fixes to include
+     * @param outputDir A directory guaranteed to be writable (e.g., app's external files dir)
+     * @return The generated zip file
+     */
+    public static File buildModule(List<FixEngine.Fix> fixes, File outputDir) throws Exception {
+        // Create temp folder inside the writable directory
+        File moduleDir = new File(outputDir, "PortDoctor_Module");
         deleteRecursive(moduleDir);
         moduleDir.mkdirs();
 
         // module.prop
-        FileWriter prop = new FileWriter(new File(moduleDir, "module.prop"));
+        File propFile = new File(moduleDir, "module.prop");
+        FileWriter prop = new FileWriter(propFile);
         prop.write("id=port_doctor_fix\n");
         prop.write("name=Port Doctor Fix Pack\n");
         prop.write("version=1.0\n");
@@ -22,7 +30,8 @@ public class ModuleBuilder {
         prop.close();
 
         // service.sh
-        FileWriter service = new FileWriter(new File(moduleDir, "service.sh"));
+        File serviceFile = new File(moduleDir, "service.sh");
+        FileWriter service = new FileWriter(serviceFile);
         service.write("#!/system/bin/sh\n");
         service.write("# Port Doctor Generated Fix Pack\n");
         service.write("# Generated: " + new Date() + "\n\n");
@@ -32,13 +41,13 @@ public class ModuleBuilder {
             service.write(fix.scriptContent);
             service.write("\n");
         }
-
         service.write("\n# All fixes applied\n");
         service.close();
-        new File(moduleDir, "service.sh").setExecutable(true);
+        serviceFile.setExecutable(true);
 
         // post-fs-data.sh
-        FileWriter postfs = new FileWriter(new File(moduleDir, "post-fs-data.sh"));
+        File postfsFile = new File(moduleDir, "post-fs-data.sh");
+        FileWriter postfs = new FileWriter(postfsFile);
         postfs.write("#!/system/bin/sh\n");
         postfs.write("# Early boot fixes\n");
         postfs.write("max_path=$(ls /sys/class/backlight/*/max_brightness 2>/dev/null | head -1)\n");
@@ -46,10 +55,11 @@ public class ModuleBuilder {
         postfs.write("    echo 4095 > \"$max_path\"\n");
         postfs.write("fi\n");
         postfs.close();
-        new File(moduleDir, "post-fs-data.sh").setExecutable(true);
+        postfsFile.setExecutable(true);
 
         // README
-        FileWriter readme = new FileWriter(new File(moduleDir, "README.txt"));
+        File readmeFile = new File(moduleDir, "README.txt");
+        FileWriter readme = new FileWriter(readmeFile);
         readme.write("Port Doctor Fix Pack\n");
         readme.write("====================\n\n");
         readme.write("Applied Fixes:\n");
@@ -60,8 +70,8 @@ public class ModuleBuilder {
         readme.write("Flash this module in Magisk and reboot.\n");
         readme.close();
 
-        // Zip
-        File outputFile = new File(outputPath);
+        // Create zip file
+        File outputFile = new File(outputDir, "PortDoctor_FixPack.zip");
         FileOutputStream fos = new FileOutputStream(outputFile);
         ZipOutputStream zos = new ZipOutputStream(fos);
 
@@ -77,9 +87,11 @@ public class ModuleBuilder {
             fis.close();
             zos.closeEntry();
         }
-
         zos.close();
         fos.close();
+
+        // Cleanup temp folder
+        deleteRecursive(moduleDir);
 
         return outputFile;
     }
