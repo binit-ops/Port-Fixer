b0567315c371010e8bf4765281328893c7da15b6
Binit <jaiswalbinit605@gmail.com>
2026-06-14 16:40:38 +0530

    Update CloudSync.java

--- app/src/main/java/com/realme/ portdoctor/CloudSync.java
+++ app/src/main/java/com/realme/ portdoctor/CloudSync.java
@@ -1,7 +1,9 @@
 package com.realme.portdoctor;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class CloudSync {
 
